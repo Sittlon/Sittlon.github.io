be1914618c82b601f5cd98741d5df8ff9a18f6db
Robert Sittig <robertsittig1996@web.de>
2022-07-01 08:25:56 +0200

    Version1

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,35 +1,59 @@
-  function getRepositories() 
-  {
-    user = 'Sittlon'
-    apirepo = `https://api.github.com/users/${user}`
+  var projectCount = 0;
 
-     listrepos = $.getJSON(apirepo + '/repos', function (data) {
-              console.log('data now', data)
+  function getRepositories() {
+      user = 'Sittlon'
+      apirepo = `https://api.github.com/users/${user}`
 
-              function compare(a, b) {
-                  if (a.watchers > b.watchers) {
-                      return -1
-                  }
-                  if (a.watchers < b.watchers) {
-                      return 1
-                  }
-                  return 0
+      listrepos = $.getJSON(apirepo + '/repos', function(data) {
+          function compare(a, b) {
+              if (a.watchers > b.watchers) {
+                  return -1
               }
+              if (a.watchers < b.watchers) {
+                  return 1
+              }
+              return 0
+          }
 
-              data.sort(compare)
-              data.forEach(v => {
-                  slider = document.getElementById("slider-projects")
-                  listItemRepo = document.createElement('li')
+          data.sort(compare)
+          for (const [i, value] of data.entries()) {
+              if (value.name == "Sittlon.github.io")
+                  break;
+              slider = document.getElementById("slider-projects")
+              listItemRepo = document.createElement('li')
+              if (i == 1) {
+                  listItemRepo.setAttribute("class", "slider--item slider--item-left")
+              }
+              if (i == 0) {
                   listItemRepo.setAttribute("class", "slider--item slider--item-center")
-                  a = document.createElement('a')
-                  p1 = document.createElement('p')
-                  p2 = document.createElement('p')
+              }
+              if (i == 2) {
+                  listItemRepo.setAttribute("class", "slider--item slider--item-right")
+              }
+              if (i > 2) {
+                  listItemRepo.setAttribute("class", "slider--item slider--item")
+              }
+              a = document.createElement('a')
+              p1 = document.createElement('p')
+              p2 = document.createElement('p')
+              div = document.createElement('div')
+              img = document.createElement('img')
 
-                  p1.innerHTML = v.name
-                  a.appendChild(p1)
-                  a.appendChild(p2)
-                  listItemRepo.appendChild(a)
-                  slider.appendChild(listItemRepo)
-              });
-          });
+              div.setAttribute("class", "slider--item-image")
+              img.setAttribute("src", "assets/img/" + value.name + ".png")
+              p1.setAttribute("class", "slider--item-title")
+              p2.setAttribute("class", "slider--item-description")
+
+              p1.innerHTML = value.name
+              p2.innerHTML = value.description
+
+              div.appendChild(img)
+              a.appendChild(div)
+              a.appendChild(p1)
+              a.appendChild(p2)
+              listItemRepo.appendChild(a)
+              slider.appendChild(listItemRepo)
+          }
+
+      });
   }
