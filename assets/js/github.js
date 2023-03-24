5dbf49d9b60daad72d2eaa9dc6539bc56509fad6
Robert Sittig <robertsittig1996@web.de>
2023-03-24 21:22:20 +0100

    .

--- assets/js/github.js
+++ assets/js/github.js
@@ -19,7 +19,7 @@
 
           data.sort(compare)
           for (const [i, value] of data.entries()) {
-              if (value.name == "Sittlon.github.io" || value.name = "github-readme-stats" || value.name == "Sittlon")
+              if (value.name == "Sittlon.github.io" || value.name == "github-readme-stats" || value.name == "Sittlon")
                   break;
               slider = document.getElementById("slider-projects")
               listItemRepo = document.createElement('li')
@@ -39,20 +39,16 @@
               p1 = document.createElement('p')
               p2 = document.createElement('p')
               div = document.createElement('div')
-              img = document.createElement('img')
-
 
               a.setAttribute("href", value.html_url)
               a.setAttribute("target", "_blank")
               div.setAttribute("class", "slider--item-image")
-                  //img.setAttribute("src", "assets/img/" + value.name + ".png")
               p1.setAttribute("class", "slider--item-title")
               p2.setAttribute("class", "slider--item-description")
 
               p1.innerHTML = value.name
               p2.innerHTML = value.description
 
-              div.appendChild(img)
               a.appendChild(div)
               a.appendChild(p1)
               a.appendChild(p2)
